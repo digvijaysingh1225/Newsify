095253f0e2f79f2fa9d027228629b2af6d149933
digvijaysingh1225 <104089746+digvijaysingh1225@users.noreply.github.com>
2023-07-01 10:37:39 +0530

    Update App.js

--- newsify/src/App.js
+++ newsify/src/App.js
@@ -13,22 +13,22 @@ function App() {
   })
   return (
     <>
-    
+
       <div className="container my-5">
         <div className="row text-center">
           {
-            news.map((val)=>{
-              return(
+            news.map((val) => {
+              return (
                 <div className="col my-3">
-            <div className="card" style={{width: "18rem"}}>
-              <img src={val.urlToImage} class="card-img-top" alt="..." />
-                <div className="card-body">
-                  <h5 className="card-title">{val.title}</h5>
-                  <p className="card-text">{val.description}</p>
-                  <button className='btn btn-primary text-dark'><a className="text-white" href={val.url}>Know More</a></button>
+                  <div className="card" style={{ width: "18rem" }}>
+                    <img src={val.urlToImage} class="card-img-top" alt="..." />
+                    <div className="card-body">
+                      <h5 className="card-title">{val.title}</h5>
+                      <p className="card-text">{val.description}</p>
+                      <button className='btn btn-primary text-dark'><a className="text-white" href={val.url}>Know More</a></button>
+                    </div>
+                  </div>
                 </div>
-            </div>
-          </div>
               )
             })
           }
